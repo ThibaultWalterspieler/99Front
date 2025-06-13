f6c257bf9feec30b939b113f8fefb462b6ad5a3f
Thibault Walterspieler <twalterspieler@pm.me>
2025-06-13 11:28:36 +0200

    feat: add @vercel/analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,34 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import { FC, PropsWithChildren } from "react";
+import { Analytics } from '@vercel/analytics/next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import { FC, PropsWithChildren } from 'react';
 
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 
-import "./globals.scss";
+import './globals.scss';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "99stud - Creative Independent Studio",
-  description: "99stud is a creative independent studio that makes things.",
+  title: '99stud - Creative Independent Studio',
+  description: '99stud is a creative independent studio that makes things.',
 };
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
