b04d3c96864774247b326fe08f915e2ea6e83c13
Thibault Walterspieler <twalterspieler@pm.me>
2025-06-13 10:43:47 +0200

    fix: fix webgl context import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import { WebGLContext } from "@99Stud/components/WebGLContext"
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const WebGLContext = dynamic(
+  () =>
+    import('@99Stud/components/WebGLContext').then((mod) => mod.WebGLContext),
+  { ssr: false }
+);
 
 export default function Home() {
-
   return (
-    <div className="relative w-full h-screen overflow-hidden">
+    <div className='relative w-full h-screen overflow-hidden'>
       <WebGLContext />
     </div>
-  )
+  );
 }
